890aed3c276978cca872635edbac7e0e703756dd
Zoe <159435044+Mortiton@users.noreply.github.com>
2024-03-27 21:29:01 +0000

    Add messageCreate event files
    Added a messageCreate event file that handles messages with the ! prefix
    these are the commands of the bot

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -1,2 +1,31 @@
-import { Message } from 'discord.js';
+import { Message } from "discord.js";
+import commands from "../commandLoader";
 
+export const messageCreate = async (message: Message) => {
+  //Ignore bot messages
+  if (message.author.bot) return;
+
+  //Ignore messages without the ! prefix
+  const prefix = "!";
+  if (!message.content.startsWith(prefix)) return;
+
+  //Extract the command name and arguments from the message
+  const args = message.content.slice(prefix.length).trim().split(/ +/);
+  const commandName = args.shift()?.toLowerCase();
+
+  //Check commandName is not undefined
+  if (!commandName) return;
+
+  //Get the command from the commands collection
+  const command = commands.get(commandName);
+
+  //if the command exists, execute it
+  if (command) {
+    try {
+      await command.execute(message, args);
+    } catch (error) {
+      console.error(error);
+      message.reply("There was an error executing that command.");
+    }
+  }
+};
