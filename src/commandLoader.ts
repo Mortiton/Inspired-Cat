5299352a53fad369eca16f3358649cebd09a2313
Zoe <159435044+Mortiton@users.noreply.github.com>
2024-03-27 21:21:46 +0000

    Add commandLoader files
    Command loader dynamically loads the command files

--- src/commandLoader.ts
+++ src/commandLoader.ts
@@ -1,2 +1,19 @@
-import { Collection } from 'discord.js';
-import client from './client';
+import fs from 'fs';
+import path from 'path'
+import { Command } from './interfaces/Command';
+
+//Creating a map to hold the commands
+const commands = new Map<string, Command>();
+
+//Read the command files
+const commandFiles = fs.readdirSync(path.join(__dirname, 'commands')).filter(file => file.endsWith('.ts'));
+
+for (const file of commandFiles) {
+    //Import command files
+    const command: Command = require(`./commands/${file}`).default;
+
+    //Add the commands to the map
+    commands.set(command.name, command);
+}
+
+export default commands;
